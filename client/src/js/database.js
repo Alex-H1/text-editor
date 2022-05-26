c166c72693f30f992d969827ae4704b99e33d0b4
alex hernandez <alexanderohernandez21@gmail.com>
2022-05-26 09:13:09 -0700

    trouble shooting

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,24 +14,25 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => { 
-  const jateDb = await openDB('jate', 1);
+  console.log('put to the database')
+  const jateDb = await openDB('jate',1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ todo: content });
+  const request = store.put({content: content});
   const result = await request;
   console.log( 'data saved to the database', result)
-  console.error('putDb not implemented')
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const jateDb = await openDB('jate', 1);
+   console.log('get  from db')
+  const jateDb = await openDB('jate',1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
-  console.log( 'result.values', result)
-  console.error('getDb not implemented');
+  console.log( 'result.values', result);
+  return result;
 }
 
 initdb();
--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,8 +1,7 @@
 // Import methods to save and get data from the indexedDB database in './database.js'
 import { getDb, putDb } from './database';
 import { header } from './header';
-
-export default class {
+export default class {                                                                              
   constructor() {
     const localData = localStorage.getItem('content');
 
@@ -22,11 +21,16 @@ export default class {
       tabSize: 2,
     });
 
+    function getContent(dataArr){
+      return dataArr.content;
+    }
+
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+      let dataStr = data.map(getContent).join('');
+      this.editor.setValue(dataStr || localData || header);
     });
 
     this.editor.on('change', () => {
